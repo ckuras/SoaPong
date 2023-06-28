b7106084250d3b1d7bc3e2d32a71df7faf98ab0c
Christopher Kuras <kuras.chris@gmail.com>
2023-06-27 22:07:46 -0400

    Added angled bounces

--- ball.gd
+++ ball.gd
@@ -1,16 +1,20 @@
 extends Area2D
 
 @export var speed = 100
-var direction = 1
+var velocity: Vector2 = Vector2(-1,0)
 
 func _ready():
 	pass
 
 func _process(delta):
-	var velocity = Vector2.ZERO
-	velocity.x -= direction
 	position += velocity * delta * speed
 
 func _on_area_entered(area):
-	print('bounce')
-	direction *= -1
+	if (area.get_groups().has("wall")):
+		velocity.y *= -1
+	else:
+		var new_velocity: Vector2 = position - area.position
+		var new_velocity_normalized = new_velocity.normalized()
+		print(new_velocity_normalized)
+		velocity = new_velocity_normalized
+	
