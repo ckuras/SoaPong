6c59374e47fbdad78e06f0a4bb45a8fb77b3c0a5
Christopher Kuras <kuras.chris@gmail.com>
2023-06-27 21:38:17 -0400

    Added ball bounce on collision

--- ball.gd
+++ ball.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
 @export var speed = 100
+var direction = 1
 
 func _ready():
 	pass
 
-
 func _process(delta):
 	var velocity = Vector2.ZERO
-	
-	velocity.x -= 1
-
+	velocity.x -= direction
 	position += velocity * delta * speed
+
+func _on_area_entered(area):
+	print('bounce')
+	direction *= -1
