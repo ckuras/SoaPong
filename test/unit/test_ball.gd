024df5cce24f9df3a5a7c7aee43edbdda1cba6d6
Christopher Kuras <kuras.chris@gmail.com>
2023-06-29 08:20:04 -0400

    Added tests for ball colliding with paddle

--- test/unit/test_ball.gd
+++ test/unit/test_ball.gd
@@ -10,9 +10,9 @@ func before_each():
 	_ball = ball.new()
 	
 	area.set_position(Vector2(0, 0))
-	area.add_to_group("wall")
 
 func test_wall_hit_turns_positive_y_velocity_negative():
+	area.add_to_group("wall")
 	var current_velocity = Vector2(1, 1)
 	
 	var new_velocity = _ball.velocity_from_wall_collision(area, current_velocity)
@@ -20,8 +20,23 @@ func test_wall_hit_turns_positive_y_velocity_negative():
 	assert_eq(Vector2(1, -1), new_velocity)
 	
 func test_wall_hit_turns_negative_y_velocity_positive():
+	area.add_to_group("wall")
 	var current_velocity = Vector2(1, -1)
 	
 	var new_velocity = _ball.velocity_from_wall_collision(area, current_velocity)
 	
 	assert_eq(Vector2(1, 1), new_velocity)
+
+func test_paddle_hit_directly_center():
+	_ball.set_position(Vector2(1, 0))
+	var current_velocity = Vector2(1, 0)
+	
+	var new_velocity = _ball.velocity_from_wall_collision(area, current_velocity)
+	assert_eq(Vector2(1, 0), new_velocity)
+
+func test_paddle_hit_from_bottom():
+	_ball.set_position(Vector2(1, 1))
+	var current_velocity = Vector2(1, 0)
+	
+	var new_velocity = _ball.velocity_from_wall_collision(area, current_velocity)
+	assert_almost_eq(Vector2(0.707, 0.707), new_velocity, Vector2(.01, .01))
